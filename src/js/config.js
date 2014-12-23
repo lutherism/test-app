308671a3f45019d51214bbac1e942745c3729c78
lutherism <xanderjanz@gmail.com>
2014-12-22 17:52:47 -0800

    ignore bower_compoents/ 	modified:   .gitignore 	modified:   bower.json 	deleted:    bower_components/jsx-requirejs-plugin/.bower.json 	deleted:    bower_components/jsx-requirejs-plugin/.gitignore 	deleted:    bower_components/jsx-requirejs-plugin/LICENSE 	deleted:    bower_components/jsx-requirejs-plugin/README.md 	deleted:    bower_components/jsx-requirejs-plugin/bower.json 	deleted:    bower_components/jsx-requirejs-plugin/index-built.html 	deleted:    bower_components/jsx-requirejs-plugin/index.html 	deleted:    bower_components/jsx-requirejs-plugin/js/JSXTransformer.js 	deleted:    bower_components/jsx-requirejs-plugin/js/jsx.js 	deleted:    bower_components/require/.bower.json 	deleted:    bower_components/require/.gitignore 	deleted:    bower_components/require/.npmignore 	deleted:    bower_components/require/Gruntfile.js 	deleted:    bower_components/require/README.md 	deleted:    bower_components/require/bin/index.js 	deleted:    bower_components/require/bin/requireify.js 	deleted:    bower_components/require/bin/templates/buffer.ejs 	deleted:    bower_components/require/bin/templates/process.ejs 	deleted:    bower_components/require/bin/templates/template.ejs 	deleted:    bower_components/require/build/require.js 	deleted:    bower_components/require/build/require.min.js 	deleted:    bower_components/require/examples/loading/README.md 	deleted:    bower_components/require/examples/loading/index.html 	deleted:    bower_components/require/examples/loading/js/index.js 	deleted:    bower_components/require/examples/loading/js/index.min.js 	deleted:    bower_components/require/examples/loading/js/lib/others/add.js 	deleted:    bower_components/require/examples/loading/js/lib/others/sub.js 	deleted:    bower_components/require/examples/loading/js/lib/test.js 	deleted:    bower_components/require/examples/loading/js/lib/workerify.js 	deleted:    bower_components/require/examples/loading/package.json 	deleted:    bower_components/require/examples/loading/route/index.js 	deleted:    bower_components/require/package.json 	deleted:    bower_components/require/src/index.js 	deleted:    bower_components/require/src/module.js 	deleted:    bower_components/requirejs-text/.bower.json 	deleted:    bower_components/requirejs-text/LICENSE 	deleted:    bower_components/requirejs-text/README.md 	deleted:    bower_components/requirejs-text/package.json 	deleted:    bower_components/requirejs-text/text.js 	modified:   server/mongo/bin/data/db/journal/lsn 	modified:   src/js/config.js

--- src/js/config.js
+++ src/js/config.js
@@ -3,11 +3,9 @@ require.config({
     paths: {
       "react": "../../bower_components/react/react-with-addons",
       "JSXTransformer": "../../bower_components/react/JSXTransformer",
-      "jsx": "../../bower_components/jsx-requirejs-plugin/js/jsx",
       "backbone": "../../bower_components/backbone/backbone",
       "underscore": "../../bower_components/underscore/underscore",
       "jquery": "../../bower_components/jquery/dist/jquery",
-      "text": "../../bower_components/requirejs-text/text",
       "dispatcher": "../../bower_components/PG-Flux/src/dispatcher",
       "invariant": "../../bower_components/PG-Flux/src/invariant",
       "env": "../json/env"
